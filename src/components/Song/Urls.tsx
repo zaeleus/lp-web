6bf63d91855b540c7e4394de1ca3d41b4a2bdff0
Michael Macias <zaeleus@gmail.com>
2017-06-05 03:12:24 -0500

    Add key attribute to Song/Urls list item

--- src/components/Song/Urls.tsx
+++ src/components/Song/Urls.tsx
@@ -7,8 +7,8 @@ interface IProps {
 }
 
 const Urls: React.StatelessComponent<IProps> = ({ urls }) => {
-    const items = urls.map((u: ISongUrl) => (
-        <li><a href={u.url}>{u.name}</a></li>
+    const items = urls.map((u: ISongUrl, i: number) => (
+        <li key={i}><a href={u.url}>{u.name}</a></li>
     ));
 
     return <ul>{items}</ul>;
