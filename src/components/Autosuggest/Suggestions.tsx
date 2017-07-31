c78a29481a334f09dbb3fe074f32a4362b9dbb07
Michael Macias <zaeleus@gmail.com>
2017-07-31 15:21:07 -0500

    Add keyboard controls for autosuggest selection

--- src/components/Autosuggest/Suggestions.tsx
+++ src/components/Autosuggest/Suggestions.tsx
@@ -3,18 +3,32 @@ import * as React from "react";
 import { IArtist } from "../../models/Artist";
 import Name from "../Name";
 
+import "./Suggestions.css";
+
 interface IProps {
+    activeIndex: number;
     artists: IArtist[];
+    isOpen: boolean;
 }
 
-const Suggestions: React.StatelessComponent<IProps> = ({ artists }) => {
+const Suggestions: React.StatelessComponent<IProps> = ({ activeIndex, artists, isOpen }) => {
+    if (!isOpen) {
+        return null;
+    }
+
     if (!artists) {
         return <li>No results.</li>;
     }
 
-    const items = artists.map((a, i) => (
-        <li key={i}><Name names={a.names} /></li>
-    ));
+    const items = artists.map((a, i) => {
+        const className = (i === activeIndex) ? "active" : "";
+
+        return (
+            <li key={i} className={className}>
+                <Name names={a.names} />
+            </li>
+        );
+    });
 
     return <ul>{items}</ul>;
 };
--- src/components/Autosuggest/index.tsx
+++ src/components/Autosuggest/index.tsx
@@ -9,27 +9,87 @@ interface IProps {
 }
 
 interface IState {
+    activeIndex: number;
+    isOpen: boolean;
     suggestions: IArtist[];
 }
 
 class Autosuggest extends React.Component<IProps, IState> {
     public constructor(props: IProps) {
         super(props);
-        this.state = { suggestions: [] };
+
+        this.state = {
+            activeIndex: 0,
+            isOpen: false,
+            suggestions: [],
+        };
     }
 
     public render() {
         return (
             <div className="autosuggest">
-                <input type="text" onChange={this.onQueryChange} />
-                <Suggestions artists={this.state.suggestions} />
+                <input type="text"
+                    onChange={this.onQueryChange}
+                    onKeyDown={this.onKeyDown}
+                    onKeyUp={this.onKeyUp} />
+                <Suggestions
+                    artists={this.state.suggestions}
+                    activeIndex={this.state.activeIndex}
+                    isOpen={this.state.isOpen} />
             </div>
         );
+    }
+
+    private onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        switch (event.keyCode) {
+            case 13: // enter
+                event.preventDefault();
+                break;
+            case 27: // escape
+                this.close();
+                break;
+        }
+    }
+
+    private onKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        switch (event.keyCode) {
+            case 38: // up arrow
+                this.open();
+                this.prevItem();
+                break;
+            case 40: // down arrow
+                this.open();
+                this.nextItem();
+                break;
+        }
     }
 
     private onQueryChange = (event: React.FormEvent<HTMLInputElement>) => {
         const query = event.currentTarget.value;
         this.fetchSuggestions(query);
+    }
+
+    private open() {
+        this.setState({ isOpen: true });
+    }
+
+    private close() {
+        this.setState({
+            activeIndex: 0,
+            isOpen: false,
+        });
+    }
+
+    private prevItem() {
+        let i = this.state.activeIndex - 1;
+        if (i < 0) { i = this.state.suggestions.length - 1; }
+        this.setState({ activeIndex: i });
+    }
+
+    private nextItem() {
+        let i = this.state.activeIndex + 1;
+        if (i >= this.state.suggestions.length) { i = 0; }
+        this.setState({ activeIndex: i });
     }
 
     private fetchSuggestions = async (query: string) => {
@@ -61,6 +121,7 @@ class Autosuggest extends React.Component<IProps, IState> {
         const suggestions = json.data.artists as IArtist[];
 
         this.setState({ suggestions });
+        this.open();
     }
 }
 
